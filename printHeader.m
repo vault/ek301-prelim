4cf6ce509bb7792fa44bce215ad39e6af7c0ecaf
Michael Abed <michaelabed@gmail.com>
2012-04-10 00:57:37 -0300

    id no

--- printHeader.m
+++ printHeader.m
@@ -2,6 +2,6 @@ function [] = printHeader()
     fprintf('%% EK301, Section A1, Group 7, Team GNU, 4/09/12\n');
     fprintf('%% Michael Abed, ID U55178419\n');
     fprintf('%% Joseph Stone, ID U44434740\n');
-    fprintf('%% Ben Havey, ID \n');
+    fprintf('%% Ben Havey, ID U46525972 \n');
     
 end
